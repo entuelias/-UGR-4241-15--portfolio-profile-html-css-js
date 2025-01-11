f1262a6072e7bad364fd3f40eb7aaef64a6eab6f
entuelias <152762908+entuelias@users.noreply.github.com>
2025-01-11 12:29:28 +0300

    Delete src directory

--- src/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleMenu = document.querySelector(".toggle-menu");
-    const navLinks = document.querySelector(".nav-links");
-  
-    toggleMenu.addEventListener("click", () => {
-      navLinks.classList.toggle("active");
-    });
-  });
-  
